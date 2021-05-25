f1b14bb97c2f61e73a01f4e6ea0084eebba1756e
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2021-05-25 18:14:36 +0700

    Переименовал решение. Добавил новую модель заказа

---
+++ book-shop/book-shop/src/models/BSModOrder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.src.models
+{
+    /// <summary>
+    /// Класс сущности заказ
+    /// </summary>
+    public class BSModOrder
+    {
+        /// <summary>
+        /// Книга
+        /// </summary>
+            public BSModBook Book { get; set; }
+
+        /// <summary>
+        /// Дата заказка
+        /// </summary>
+            public DateTime DateStart { get; set; }
+
+        /// <summary>
+        /// Дата исполнения
+        /// </summary>
+            public DateTime DateEnd { get; set; }
+
+        /// <summary>
+        /// Количество
+        /// </summary>
+            public ushort Count { get; set; }
+
+        /// <summary>
+        /// Сотрудник
+        /// </summary>
+            public BSModManager Worker { get; set; }
+    }
+}
