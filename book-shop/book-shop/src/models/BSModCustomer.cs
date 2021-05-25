405bd958c9403b03fc86909689b6e6935e3d9775
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2021-05-25 18:08:25 +0700

    Добавил модель заказчика

---
+++ book-shop/book-shop/src/models/BSModCustomer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_shop.src.models
+{
+    /// <summary>
+    /// Класс сущности заказчика
+    /// </summary>
+    public class BSModCustomer
+    {
+        /// <summary>
+        /// Имя заказчика
+        /// </summary>
+            public string Name { get; set; }
+
+        /// <summary>
+        /// Фамилия заказчика
+        /// </summary>
+            public string SurName { get; set; }
+
+        /// <summary>
+        /// Номер телефона
+        /// </summary>
+            public string PhoneNumber { get; set; }
+    }
+}
