202419c50ea4216beb511d765838b68ab9f126b7
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2021-05-25 16:51:56 +0700

    Закончил создание модели издательсва. Создал новыу модель для менеджера

---
+++ book-shop/book-shop/src/models/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.src.models
+{
+    public class Manager
+    {
+        /// <summary>
+        /// Имя
+        /// </summary>
+            public string Name { get; set; }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+            public string SurName { get; set; }
+
+        /// <summary>
+        /// Пол
+        /// </summary>
+            public string Sex { get; set; }
+    }
+}
--- book-shop/book-shop/src/models/PublishingHouse.cs
+++ book-shop/book-shop/src/models/PublishingHouse.cs
@@ -16,9 +16,28 @@ namespace BookShop.src.models
             public int ID { get; set; }
 
         /// <summary>
+        /// Адрес
+        /// </summary>
+            public string Address { get; set; }
+
+        /// <summary>
         /// Название издательства
         /// </summary>
             public string Name { get; set; }
 
+        /// <summary>
+        /// Номер телефона
+        /// </summary>
+            public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Факс
+        /// </summary>
+            public string FaxNumber { get; set; }
+
+        /// <summary>
+        /// Менеджер
+        /// </summary>
+            public Manager Manager { get; set; }
     }
 }
